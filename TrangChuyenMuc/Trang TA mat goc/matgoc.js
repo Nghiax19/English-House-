69ec92533fc8a4dfa49d85d977df237a003ebe99
TUANNN <tuannnm510@gmail.com.com>
2024-06-17 22:31:28 +0700

    update

---
+++ TrangChuyenMuc/Trang TA mat goc/matgoc.js
@@ -0,0 +1,8 @@
+// function showContent(id) {
+//     var contents = document.getElementsByClassName('focus-text');
+//     for (var i = 0; i < contents.length; i++) {
+//         contents[i].style.display = 'none';
+//     }
+//     document.getElementById(id).style.display = 'block';
+// }
+
